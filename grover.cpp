018ac79f040c00e6e536e91beabab048bc05982d
cykim <cykim8811@snu.ac.kr>
2024-08-09 17:08:02 +0000

    Initial Commit

---
+++ grover.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "quantum.h"
+
+using namespace std;
+
+void oracle(vector<Qubit*>& qubits) {
+    vector<Qubit*> controls = {qubits[0], qubits[1], qubits[2]};
+    qubits[2]->x();
+    qubits[3]->mcx(controls);
+    qubits[2]->x();
+}
+
+void diffusion(vector<Qubit*>& qubits) {
+    vector<Qubit*> qubits_view(qubits.begin(), qubits.begin() + 3);
+    for (auto& qubit : qubits_view) {
+        qubit->h();
+    }
+    for (auto& qubit : qubits_view) {
+        qubit->x();
+    }
+    qubits_view[2]->mcz({qubits_view[0], qubits_view[1]});
+    for (auto& qubit : qubits_view) {
+        qubit->x();
+    }
+    for (auto& qubit : qubits_view) {
+        qubit->h();
+    }
+}
+
+int main() {
+    const int n_qubits = 3;
+    const int n_iterations = floor(M_PI/4 * sqrt(pow(2, n_qubits)));
+    
+    vector<Qubit*> qubits;
+    for (int i = 0; i < n_qubits; i++) {
+        qubits.push_back(new Qubit(false));
+    }
+    
+    qubits.push_back(new Qubit(true));
+    
+    for (auto& qubit : qubits) {
+        qubit->h();
+    }
+    
+    for (int i = 0; i < n_iterations; i++) {
+        oracle(qubits);
+        diffusion(qubits);
+    }
+
+    qubits[0]->_display();
+    
+    cout << "Measured state: ";
+    for (int i = 0; i < n_qubits; i++) {
+        cout << qubits[i]->measure();
+    }
+    cout << endl;
+    
+    for (auto& qubit : qubits) {
+        delete qubit;
+    }
+    
+    return 0;
+}
---
+++ quantum.h
@@ -0,0 +1,56 @@
+#ifndef QUANTUM_H
+#define QUANTUM_H
+
+#include <vector>
+#include <complex>
+
+using namespace std;
+
+class QubitState;
+
+class Qubit {
+    private:
+        QubitState* state;
+
+    public:
+        Qubit();
+        Qubit(bool value);
+        ~Qubit();
+        bool measure();
+        void h();
+        void x();
+        void cx(Qubit& control);
+        void mcx(vector<Qubit*> controls);
+        void mcz(vector<Qubit*> controls);
+        void phase(float angle);
+        void operator^=(Qubit &q);
+        Qubit& operator!();
+        Qubit& operator=(Qubit &q);
+        Qubit& operator=(bool value);
+        void _display();
+    friend class QubitState;
+};
+
+class QubitState {
+    private:
+        vector<Qubit*> qubits;
+        vector<complex<float>> amplitudes;
+        void entangle(Qubit* qubit);
+        void entangle(vector<Qubit*> qubits);
+    public:
+        QubitState(Qubit* qubit);
+        ~QubitState();
+        bool measure(Qubit* target);
+        void h(Qubit* target);
+        void x(Qubit* target);
+        void cx(Qubit* control, Qubit* target);
+        void mcx(vector<Qubit*> controls, Qubit* target);
+        void mcz(vector<Qubit*> controls, Qubit* target);
+        void phase(Qubit* target, float angle);
+        void remove(Qubit* qubit);
+        void product(QubitState* other);
+        void _display();
+    friend class Qubit;
+};
+
+#endif // QUANTUM_H
